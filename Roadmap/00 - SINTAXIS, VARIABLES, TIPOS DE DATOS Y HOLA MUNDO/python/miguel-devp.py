5265c0fae3841325e8c9b065cfff1a55f3cb2a6f
Michel <miguelangel@pixelandpixel.com>
2025-04-16 17:10:17 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/miguel-devp.py
@@ -0,0 +1,19 @@
+# https://www.python.org/
+
+# Comentario de una línea
+
+"""
+Comentario en 
+varias 
+líneas
+"""
+
+my_var = 'Miguel'
+
+var_string = 'Hola'
+var_int = 1
+var_float = 2.5
+var_bool = True
+
+language = 'Python'
+print(f'¡Hola, {language}')
